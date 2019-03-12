b6054e7b8fd869df339bec01d97e4a91c31c8faf
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-12 16:11:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,13 +1,7 @@
 def sort_array_asc(array)
-  array.sort do |a, b|
-    if a == b
-      0
-    elsif a < b
-      -1
-    elsif a > b
-      1
+    array.sort do |a, b|
+      a <=> b
     end
-  end
 end
 
 def sort_array_desc(array)
